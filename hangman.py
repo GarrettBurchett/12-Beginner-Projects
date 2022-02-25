17f454692ab60caa02abd6b3352cc0a2fe751443
Garrett Burchett <gburchett88@gmail.com>
2022-02-24 19:29:42 -0600

    Initial commit with projects I've completed/are working on.

---
+++ hangman.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+
+def get_valid_word(words):
+    word = random.choice(words) # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() # what the user has guessed
+
+    # getting user input
+    user_letter = input('Guess a letter: ').upper()
+
+user_input = input('Type something: ')
+print(user_input)
