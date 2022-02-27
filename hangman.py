a385f117972d3f4db0f189cee13b3bdcc28ad1f3
Garrett Burchett <gburchett88@gmail.com>
2022-02-26 23:29:06 -0600

    added more code to hangman.

--- hangman.py
+++ hangman.py
@@ -15,8 +15,30 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
 
+    lives = 6
     # getting user input
-    user_letter = input('Guess a letter: ').upper()
+    while len(word_letters) > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have used these letters: ', ' '.join(used_letters))
 
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letter else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives = lives - 1 # takes away a life if wrond
+                print('Letter is not in word.')
+                
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+
+        else:
+            print('Invalid Character. Please try again.')
 user_input = input('Type something: ')
 print(user_input)
